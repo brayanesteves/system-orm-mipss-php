2ccfda54e1994e30432a89ba15e3157375fc9bb8
Brayan Esteves <werneriasearch@gmail.com>
2021-10-01 14:04:06 -0400

    Modify file 'helps.php' Nº2

--- libs/help/helps.php
+++ libs/help/helps.php
@@ -10,13 +10,14 @@
             }
         }
     }
+
     /**
      * This function will help us to return an 'assets' 
      * $assets: Name of the file inside the 'assets' folder
      */
     function assets($assets) {
         $urlmain = trim(str_replace("index.php", "", $_SERVER['PHP_SELF']), "/");
-        echo "/" . $urlmain . "/assets" . $assets;
+        echo "/" . $urlmain . "/assets/" . $assets;
     }
     /**
      * Function that allows redirection to another part 
@@ -25,5 +26,27 @@
     function redirect($route) {
         $urlmain = str_replace("index.php", "", $_SERVER['PHP_SELF']);
         header("location:/" . trim($urlmain, "/") . "" . $route);
+    }
+
+    /**
+     * Function that allows us to write a 'URL' through which we pass it 
+     * $route: Route to where to go
+     */
+    function url($route) {
+        $urlmain = str_replace("index.php", "", $_SERVER['PHP_SELF']);
+        echo "/" . trim($urlmain, "/") . "/" . $route;
+    }
+
+    /**
+     * Function creating the 'csrf' for validation - token
+     */
+    session_start();
+    function csrf_token() {
+        if(isset($_SESSION["token"])) {
+            unset($_SESSION["token"]);
+        }
+        $csrf_token = md5(uniqid(mt_rand(), true));
+        $_SESSION["csrf_token"] = $csrf_token;
+        echo $csrf_token;
     }
 ?>
